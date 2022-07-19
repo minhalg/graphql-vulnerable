e5e2efbaa0c03ebf4607dc061fd6801f36d21c0d
Minhal Abbas Gardezi <mag166@case.edu>
2022-07-19 12:33:02 -0400

    Update insecure-db.tf

--- insecure-db.tf
+++ insecure-db.tf
@@ -8,5 +8,5 @@ resource "aws_db_instance" "default" {
   password             = "foobarbaz"
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
-  publicly_accessible = false
+  publicly_accessible = true
 }
